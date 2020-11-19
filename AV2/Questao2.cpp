d2d1d97771114252bb09d7f3e0e715ab1f23186b
Daniel Ferraz <daniel0ferraz@outlook.com>
2020-11-18 22:21:32 -0300

    adicionado  q1 e q2 av2 algoritmos

--- AV2/Questao2.cpp
+++ AV2/Questao2.cpp
@@ -1,18 +1,16 @@
 #include <stdio.h>
-int main()
-{
-  int s[5];
-  int i, j;
-
-  for (i = 1; i <= 5; i++)
-  {
-    s[i] = 0;
-    for (j = 1; j <= i; j++)
-    {
-      s[i] = s[i] + j;
+#include <stdlib.h>
+main() {
+    int vet[5];
+    
+    for(int i = 0; i < 5; i++) {
+        vet[i] = 0;
+        for(int j = 0; j <= i; j++) {
+            vet[i] += j + 1;
+        }
     }
-  }
-
-  for (i = 1; i <= 5; i++)
-    printf("Resultado: %d\n", s[i] - s[5 - i + 1]);
+    printf("\n");
+    for(int i = 0; i < 5; i++) {
+        printf("%i ", vet[i] - vet[5 - i - 1]);
+    }
 }
--- TEMA-2 VETOR/ex3.cpp
+++ TEMA-2 VETOR/ex3.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 main()
 {
    int i, vetor[3];
