2b70b81a524812218c9e6b5163af7f38c47f613f
Daniel Ferraz <daniel0ferraz@outlook.com>
2020-12-01 10:09:33 -0300

    update exercicios

--- TEMA-3 REGISTROS (struct)/Ex2.cpp
+++ TEMA-3 REGISTROS (struct)/Ex2.cpp
@@ -1,35 +1,35 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct aluno {
+struct aluno
+{
 	char nome[10];
-	float P1,P2;
+	float P1, P2;
 	int faltas;
 };
 
-main() {
+main()
+{
 	struct aluno alunos[5];
-	 int i;
-	 for(i=0;i<5;i++) 
-	 {
-	 	system("cls");
+	int i;
+	for (i = 0; i < 5; i++)
+	{
+		system("cls");
 		printf("digite o nome:\n");
-		scanf("%s",alunos[i].nome);
+		scanf("%s", alunos[i].nome);
 		printf("digite a primeira nota:\n");
-		scanf("%f",&alunos[i].P1);
+		scanf("%f", &alunos[i].P1);
 		printf("digite a segunda nota:\n");
-		scanf("%f",&alunos[i].P2);
+		scanf("%f", &alunos[i].P2);
 		printf("digite a quantidade de faltas:\n");
-		scanf("%d",&alunos[i].faltas);
-	 }
-	 
-	 for(i=0;i<5;i++)
-	 {
-	 	printf("%s\n", alunos[i].nome);
+		scanf("%d", &alunos[i].faltas);
+	}
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("%s\n", alunos[i].nome);
 		printf("%2.1f\n", alunos[i].P1);
 		printf("%2.1f\n", alunos[i].P2);
 		printf("%d\n", alunos[i].faltas);
-	
-	 }
+	}
 }
-
